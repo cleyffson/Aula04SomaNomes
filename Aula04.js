d6ab6e1d9f99d33b5faab39a4c23e4593484e1ad
bianca <cleyffson_silva@hotmail.com>
2022-03-04 19:51:43 -0300

    primeiro commit

--- Aula04.js
+++ Aula04.js
@@ -1,10 +1,10 @@
-const nomes = ("Ana Maria", "Antonio", "Rodrigo", "Alex", "Cristina")
+const nomes = ["Ana Maria", "Antonio", "Rodrigo", "Alex", "Cristina"];
 const res = nomes.map ((nome) => nome.charAt(0))
 console.log(res)
 
 const valores = [1,2,3,4]
 const soma = valores.reduce((ac,v) => ac + v, 0)
-console.log(soma)
+console.log(soma);
 
 
 // const todosComecamComA = nomes.every(nome => nome.startswith("A"))
